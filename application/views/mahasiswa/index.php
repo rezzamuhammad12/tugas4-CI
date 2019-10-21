031f1d219899c165ad530b7f879eb46a5c60b0c3
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-10-22 06:46:00 +0700

    Menambahkan pemberitahuan/alert pada saat data berhasil ditambahkan

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,4 +1,17 @@
 <div class="container">
+    <?php if ( $this->session->flashdata('flash') ) : ?>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">Data mahasiswa
+        <strong>berhasil</strong> <?=  $this->session->flashdata('flash'); ?>.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+
     <div class="row mt-3">
         <div class="col-md-6">
             <a href="<?= base_url();?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>

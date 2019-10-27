bc748b33c408e5280dbd7b25bbe8fb59b9996f6a
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-10-27 08:51:27 +0700

    Menambahkan fitur sweetalert untuk tambah data, ubah data, hapus pada data mahasiswa

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,28 +1,27 @@
 <div class="container">
-    <?php if ( $this->session->flashdata('flash') ) : ?>
-    <div class="row mt-3">
-        <div class="col-md-6">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">Data mahasiswa
-        <strong>berhasil</strong> <?=  $this->session->flashdata('flash'); ?>.
+<div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
+    <?php if( $this->session->flashdata('flash') ) : ?>
+        <!-- <div class="row mt-3">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data Mahasiswa <strong>berhasil</strong> <?= $this->session->flashdata('flash'); ?>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-        </div>
-    </div>
+        </div> -->
     <?php endif; ?>
 
     <div class="row mt-3">
         <div class="col-md-6">
-            <a href="<?= base_url();?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
+            <a href="<?= base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
         </div>
     </div>
 
     <div class="row mt-3">
         <div class="col-md-6">
             <form action="" method="post">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Cari data mahasiswa.." name="keyword">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cari Data Mahasiswa" name="keyword">
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="submit">Cari</button>
                     </div>
@@ -33,22 +32,22 @@
 
     <div class="row mt-3">
         <div class="col-md-6">
-        <h3>Daftar Mahasiswa</h3>
-            <?php if ( empty($mahasiswa) ): ?>
+            <h3>Daftar Mahasiswa</h3>
+            <?php if( empty($mahasiswa) ) : ?>
                 <div class="alert alert-danger" role="alert">
-                    data mahasiswa tidak ditemukan.
+                    Data Mahasiswa Tidak Ditemukan
                 </div>
             <?php endif; ?>
-            <ul class="list-group">
-                <?php foreach ( $mahasiswa as $mhs) : ?>
-                <li class="list-group-item">
-                    <?= $mhs['nama']; ?>
-                        <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right" onclick="return confirm('yakin?');">hapus</a>
-                        <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right" >ubah</a>
-                        <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right" >detail</a>
+            <?php foreach( $mahasiswa as $mhs ) : ?>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <?= $mhs['nama']; ?>
+                        <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right tombol-hapus">hapus</a>
+                        <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right">ubah</a>
+                        <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right">detail</a>
                     </li>
-                <?php endforeach; ?>
-            </ul>
+                </ul>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>

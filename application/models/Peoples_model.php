30df0cc15942d49fa56b81ca80dfdb5993dcf42a
Rezza Mochamad <rezzamuhammad12@gmail.com>
2019-11-09 16:21:20 +0700

    Membenarkan program yang sebelumnya error

--- application/models/Peoples_model.php
+++ application/models/Peoples_model.php
@@ -11,7 +11,7 @@ class Peoples_model extends CI_Model
     {
         if ($keyword) {
             $this->db->like('name', $keyword);
-            $this->db-or_like('email', $keyword);
+            $this->db->or_like('email', $keyword);
         }
         return $this->db->get('peoples', $limit, $start)->result_array();
     }
